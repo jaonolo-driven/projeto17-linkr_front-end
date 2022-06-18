7a14f226b6379e17c30c88b38317b3486f8843a5
wilembergson <wilembergson@yahoo.com>
2022-06-18 18:30:34 -0300

    Saving post changes

--- src/components/PostContent/index.jsx
+++ src/components/PostContent/index.jsx
@@ -5,12 +5,15 @@ import { AiFillDelete } from "react-icons/ai";
 import { PostContent, UrlPost, UrlPostText } from "../PostsByUserPage/styles";
 import { useRef, useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
 
 export default function PostContentComponent(props){
     const {postsList, post, index} = props
+    const token = JSON.parse(localStorage.getItem('user'))
     const inputRef = useRef()
     const [editPost, setEditPost] = useState(false)
-    const [postValue, setPostValue] = useState('')
+    const [postValue, setPostValue] = useState(post.message)
+    const [resetValue, setResetValue] = useState('')
 
     useEffect(()=>{
         if(editPost){
@@ -20,12 +23,30 @@ export default function PostContentComponent(props){
 
     function changeEditPost(){
         if(editPost===false){
+            setResetValue(postValue)
             setEditPost(true)
-            setPostValue(post.message)
         }else{
             setEditPost(false)
-            setPostValue('')
+            setPostValue(resetValue)
         }
+    }
+
+    function editPostSubmit(e){
+        e.preventDefault()
+        const promise = axios.put(`${process.env.REACT_APP_API_URL}/editpost`,{
+            id: post.id,
+            message: postValue
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        promise.then(response => {
+            setEditPost(false)
+        }).catch(e => {
+            alert(e.response.data.message)
+        })
     }
     return(
         <PostContent>
@@ -36,14 +57,14 @@ export default function PostContentComponent(props){
                     <AiFillDelete margin={10}/>
                 </EditAndDel>
             </NameAndButtons> 
-            {(editPost===true)   ?  <form>
+            {(editPost===true)   ?  <form onSubmit={editPostSubmit}>
                                         <Input ref={inputRef}
                                             type='text'
                                              value={postValue}
                                              onChange={e => setPostValue(e.target.value)}
                                              />
                                     </form>
-                                    : <ReactHashtag>{post.message}</ReactHashtag>}
+                                    : <ReactHashtag>{postValue}</ReactHashtag>}
             <UrlPost>
                 <UrlPostText>
                     <h4>{postsList[index].url.title}</h4>

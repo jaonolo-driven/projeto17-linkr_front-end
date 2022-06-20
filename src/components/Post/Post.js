297833a67d14c1700545415c7fb1d87738f976dc
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-06-20 04:12:22 -0300

    feat: added hashtag-page

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,18 @@
-import ReactHashtag from "react-hashtag"; import { FaRegHeart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import ReactHashtag from "react-hashtag";
+import { FaRegHeart } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import { CreatePost, PostHTML, Photo, PostAside, SubPostAside, PostContent, UrlPost, UrlPostText} from "./styles";
 
 export default function Post(props){
     
     const { postsList } = props
-    
+    const navigate = useNavigate();
+
+    function goToHashTag(tag) {
+        navigate("/hashtag/" + tag.split("#")[1]);
+        window.location.reload();
+    }
+
     function Card(props){
         const {postINFO} = props
         return(
@@ -21,7 +28,14 @@ export default function Post(props){
                     <Link to={`/user/${postINFO.userId}`}>
                         <h3>{postINFO.userName}</h3>
                     </Link> 
-                    <p><ReactHashtag>{postINFO.message}</ReactHashtag></p>
+                    <p>
+                    <ReactHashtag renderHashtag={(tag) => (
+                        <span onClick={()=>goToHashTag(tag)}>
+                            {tag}
+                        </span>
+                    )}>
+                        {postINFO.message}
+                    </ReactHashtag></p>    
                     <UrlPost>
                         <UrlPostText>
                             <h4>{postINFO.urlMeta.url.title}</h4>
